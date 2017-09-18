d8754d6ce52bc2aa48f3e1fce052717360d9e276
Zach Panzarino <zachary@panzarino.com>
2017-09-18 13:08:47 -0400

    Notes Sep 18

--- notes/Book.java
+++ notes/Book.java
@@ -3,16 +3,20 @@ public class Book {
     private String author;
     private int numPages;
 
+    public static int bookCount = 0;
+
     public Book(String title, String author, int numPages) {
         this.title = title;
         this.author = author;
         this.numPages = numPages;
+        bookCount++;
     }
 
     public Book() {
         title = "Unknown";
         author = "Anonymous";
         numPages = 0;
+        bookCount++;
     }
 
     public String toString() {
@@ -21,5 +25,25 @@ public class Book {
 
     public boolean isLong() {
         return numPages > 300;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return numPages;
     }
 }
--- notes/BookTester.java
+++ notes/BookTester.java
@@ -1,5 +1,6 @@
 public class BookTester {
     public static void main(String[] args) {
+        int value = 23;
         Book book1 = new Book("1984", "Orwell", 274);
         Book book2 = new Book("It", "King", 429);
         Book book3 = new Book();
@@ -9,5 +10,42 @@ public class BookTester {
         System.out.println(book2.isLong());
         System.out.println(book3);
         System.out.println(book3.isLong());
+
+        Book[] books = new Book[3];
+        books[0] = new Book("Emma", "Austen", 245);
+        books[1] = book1;
+        books[2] = book2;
+
+        System.out.println(book1.getTitle());
+        book1.setTitle("Carrie");
+        System.out.println(book1);
+        changeArray(books);
+        for (Book b : books) {
+            System.out.println(b);
+        }
+        changeValue(value);
+        System.out.println(value);
+
+        System.out.println(Book.bookCount);
+
+        System.out.println(sumPages(books));
+    }
+
+    public static void changeArray(Book[] myBooks) {
+        for (int i = 0; i < myBooks.length; i++) {
+            myBooks[i].setAuthor("McDaniel");
+        }
+    }
+
+    public static void changeValue(int i) {
+        i = 99;
+    }
+
+    public static int sumPages(Book[] myBooks) {
+        int sum = 0;
+        for (Book b : myBooks) {
+            sum += b.getPages();
+        }
+        return sum;
     }
 }
