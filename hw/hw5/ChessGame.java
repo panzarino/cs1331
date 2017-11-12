87a25b16f600c05f52694572701efa46c49ba216
Zach Panzarino <zachary@panzarino.com>
2017-11-12 16:54:16 -0500

    Finish chess game

--- hw/hw5/ChessGame.java
+++ hw/hw5/ChessGame.java
@@ -9,6 +9,14 @@ public class ChessGame {
         this.moves = moves;
     }
 
+    public List<Move> getMoves() {
+        return moves;
+    }
+
+    public Move getMove(int index) {
+        return moves.get(index);
+    }
+
     public List<Move> filter(Predicate<Move> filter) {
         List<Move> output = new ArrayList<Move>();
         for (Move move : moves) {
@@ -33,5 +41,22 @@ public class ChessGame {
                     && !move.getBlackPly().getComment().isPresent();
             }
         });
+    }
+
+    public List<Move> getMovesWithPiece(Piece piece) {
+        return filter(new FilterPieces(piece));
+    }
+
+    public class FilterPieces implements Predicate<Move> {
+        private Piece piece;
+
+        public FilterPieces(Piece piece) {
+            this.piece = piece;
+        }
+
+        public boolean test(Move move) {
+            return move.getWhitePly().getPiece().getClass() == piece.getClass()
+                || move.getBlackPly().getPiece().getClass() == piece.getClass();
+        }
     }
 }
